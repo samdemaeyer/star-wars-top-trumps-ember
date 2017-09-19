75cb459eb9dfc10a78e4ba0e85f2768bc5b6e226
Sam De Maeyer <sam@showmyhomework.co.uk>
2017-09-19 11:25:38 +0100

    add criteria to readme

--- app/router.js
+++ app/router.js
@@ -7,6 +7,8 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('/star-wars-top-trumps', { path: '/*path' });
+  this.route('index', { path: '' });
   this.route('people');
   this.route('starships');
 });
